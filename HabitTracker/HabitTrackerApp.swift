d9f62b65ad42df81869a1b5651eb7c64eef3ea31
Vadim Kononenko <vadiskon@gmail.com>
2024-11-15 15:22:20 +0200

    init commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Vadim Kononenko on 15.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
